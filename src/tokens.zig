01321a53997e07f4d8d82d8874c5f2cdb07ab7b4
tusharhero <tusharhero@sdf.org>
2024-07-06 21:53:44 +0530

    add tokentypes and token struct.

---
+++ src/tokens.zig
@@ -0,0 +1,80 @@
+// The GPLv3 License (GPLv3)
+
+// Copyright © 2024 tusharhero
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+const std = @import("std");
+
+pub const TokenType = enum {
+    // Single-character tokens.
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+    // One or two character tokens.
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+    // Literals.
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+    // Keywords.
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+    // EOF is just EOF.
+    EOF,
+};
+
+pub const Token = struct {
+    _type: TokenType,
+    lexeme: []u8,
+    line: u64,
+
+    // Caller owns the string.
+    fn toString(self: Token, allocator: std.mem.Allocator) ![]u8 {
+        return try std.fmt.allocPrint(
+            allocator,
+            "{any} {s} {s}",
+            .{ self._type, self.lexeme, self.line },
+        );
+    }
+};
